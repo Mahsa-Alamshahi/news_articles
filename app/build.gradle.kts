738f9b9b457f3e75bbb4189fb40b4114a99b56a6
Mahsa Alamshahi <alamshahi.mahsa@gmail.com>
2023-08-12 11:36:21 +0330

    Retrofit dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,4 +100,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
 
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:3.6.0")
+
+
 }
